e7af0563eb8fe19062493ec810b7ec314911e27a
Rabib001 <rxh3770@mavs.uta.edu>
2025-03-31 17:11:01 -0500

    node setup

--- appwrite/config.ts
+++ appwrite/config.ts
@@ -13,12 +13,12 @@ const account = new Account(client);
 const databases = new Databases(client);
 const storage = new Storage(client);
 
-export const DATABASE_ID = '67e72aef00071555f538';
+export const DATABASE_ID = '67eafaec0002f174820f';
 export const USERS_COLLECTION_ID = 'users';
 export const LISTINGS_COLLECTION_ID = 'listings';
 export const IMAGES_COLLECTION_ID = 'images';
 export const IMAGES_BUCKET_ID = 'listing-images';
-export const CHATS_COLLECTION_ID = '67e834c60026698fc1c8';
-export const MESSAGES_COLLECTION_ID = '67e834c90039d2a6f82f';
+export const CHATS_COLLECTION_ID = '67eafb63001e983d4770';
+export const MESSAGES_COLLECTION_ID = '67eafb6600296246d0e0';
 
 export { client, account, databases, storage };
